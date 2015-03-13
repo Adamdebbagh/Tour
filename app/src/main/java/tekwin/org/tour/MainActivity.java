2b68b799de720c8c66b88c67dee5c96fdb04c1d4
Adam debbagh <adamdebbagh@gmail.com>
2015-03-13 13:18:04 -0400

    connect the app to google map services

--- app/src/main/java/tekwin/org/tour/MainActivity.java
+++ app/src/main/java/tekwin/org/tour/MainActivity.java
@@ -1,17 +1,31 @@
 package tekwin.org.tour;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Dialog;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.maps.GoogleMap;
 
 
 public class MainActivity extends ActionBarActivity {
 
+
+    private static final int GPS_ERRORDIALOG_REQUEST = 9001 ;
+    GoogleMap mMap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (serviceOk()){
+            Toast.makeText(this,"Ready to Map",Toast.LENGTH_LONG).show();
+
+        }
     }
 
 
@@ -35,5 +49,20 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public boolean serviceOk(){
+        int isAvailable = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+        if (isAvailable == ConnectionResult.SUCCESS){
+            return true;
+        }
+        else if(GooglePlayServicesUtil.isUserRecoverableError(isAvailable)){
+            Dialog dialog = GooglePlayServicesUtil.getErrorDialog(isAvailable,this,GPS_ERRORDIALOG_REQUEST);
+            dialog.show();
+        }
+        else {
+            Toast.makeText(this,"Can't connect to Google Play services",Toast.LENGTH_LONG).show();
+        }
+        return false;
     }
 }
